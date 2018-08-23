1c6250b5920d77869d4065962937215d16c916d6
robin6510 <robin6510@outlook.com>
2018-08-23 23:30:21 +0800

    re
    修改re 的py 文件, 以便提取check, 但还没完成

--- re.py
+++ re.py
@@ -2,21 +2,14 @@
 
 import sys,string,time,re
 
-
-
 #get parameters
-
-#re_op=re.compile(r'[A-Z\>][a-zA-Z\s\(]{1,30}\s[\d]{1,9}')
-
-
 f=open('w064_result.txt')
 str1=f.read()
 str2=re.findall(r"\--+(.+?)\+F3=Exit",str1)
 str3=str2[0]
 
-
+#获取结果部分
 op_result=re.findall(r'[A-Z\>][a-zA-Z\s\(]{1,30}\s[\d]{1,9}',str3)
-
 new_list=[]
 list2=[]
 for op in op_result:
@@ -26,16 +19,17 @@ for op in op_result:
     elif len(templist)>1:
         str4=' '.join(templist)
         new_list.append(str4)
-    list2.extend(re.findall(r'[\d]{1,9}$',op))    
+    list2.extend(re.findall(r'[\d]{1,9}$',op))
 
-d=dict(zip(new_list,list2))
+# 获取参数部分, 包括sold# ,两个date, product
+op_check=str1.replace(str3,'')
+str2=re.findall(r'Sold#\d{7}',op_check)
 
-
-print new_list
-print list2
-print d
-                      
-                      
+check_list=[]
 
 
+
+# sold :    Sold#\d{7}
+# date :    Start Ship Date From [\d\/+]{6,8}To [\d\/+]{6,8}
+#product :  Product....:.{1,4}Only Div
 #   [A-Z\>][a-zA-Z\s\(]{1,30}\s[\d]{1,9}
